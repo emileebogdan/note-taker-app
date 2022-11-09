8ce88d51645ae199765c33265edca562902ee8a0
emileebogdan <emileebogdan@gmail.com>
2022-11-08 17:54:46 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const routes = require('./routes/notes');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.use('/api/notes', routes);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
